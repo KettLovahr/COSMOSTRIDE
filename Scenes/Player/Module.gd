668e066fc3cf5e7ae18bf2b9c165be467925d600
Kett Lovahr <catusfelony@gmail.com>
2023-09-30 20:22:28 -0300

    Added music

---
+++ Scenes/Player/Module.gd
@@ -0,0 +1,14 @@
+extends Node
+class_name Module
+
+enum ModTypes {
+	SHIELD     ,
+	SPEED      ,
+	BARREL_ROLL,
+	SHOT_SPEED ,
+	SHOT_DAMAGE,
+	TWIN_FIRE  ,
+	REGEN      ,
+}
+
+var mod_type: ModTypes
