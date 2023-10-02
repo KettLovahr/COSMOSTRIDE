88ad13c6413fa1f207deb969b82b0a8610bd23da
Kett Lovahr <catusfelony@gmail.com>
2023-10-02 14:50:03 -0300

    Added a restart button to the Game Over screen

--- Scenes/GameOver/GameOver.gd
+++ Scenes/GameOver/GameOver.gd
@@ -4,3 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$GameOverRect/FinalScoreLabel.text = "FINAL SCORE\n%06d at level %02d" % [GameState.total_score, GameState.level]
+
+
+func _on_button_pressed():
+	GameState.restart()
